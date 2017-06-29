3d0a2b11f9f4511e7b4f47c610c3da19e515ccf4
14EC002 <govindhpavi@gmail.com>
2017-06-29 15:36:01 +0530

    Create factoreial.java

---
+++ factoreial.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class factorial{
+public static void main(String args[]){
+int n,fact=0;
+Scaneer in=new Scanner(System.in);
+n=in.nextInt();
+for(int i=0;i<=n;i++)
+{
+fact=faci*i;
+}
+System.out.println(fact)
+}
+}
